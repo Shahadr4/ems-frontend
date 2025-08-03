0f4e568cb78a53dc7ceb692081e81394b8c2189c
shahad rahman p <116180479+Shahadr4@users.noreply.github.com>
2025-08-03 18:26:29 +0530

    Update DepartmemtList.jsx

--- src/components/department/DepartmemtList.jsx
+++ src/components/department/DepartmemtList.jsx
@@ -38,7 +38,7 @@ export default function DepartmentList() {
 
   const onDepartmentDelete = async (id) => {
     try {
-      await axios.delete(`https://ems-backend-taupe.vercel.app/api/department/${id}`, {
+      await axios.delete(`https://ems-backend-chi.vercel.app/api/department/${id}`, {
         headers: {
           Authorization: `Bearer ${localStorage.getItem("token")}`,
         },
